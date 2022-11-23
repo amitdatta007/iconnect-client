099ad803bc8dfdc93d4a37f5b36e30cd8447662b
Amit Datta <ak.datta2648@gamil.com>
2022-11-24 01:27:46 +0600

    Route Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import router from "./Routes/Router/Router";
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-[98%] max-w-[1280px] bg-red-300 mx-auto">
+      <RouterProvider router={router} />
     </div>
   );
 }
